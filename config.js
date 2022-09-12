607e3f208bb947cd7694e64f6df9dfdcadaa9f07
Ellinor <ellipop2206@gmail.com>
2022-09-12 12:49:42 -0500

    Add files via upload
    AAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD_GCSRd7sEvwofTx8iINdK1a9rwSZ2NW8",
+  authDomain: "jhajssdsfff.firebaseapp.com",
+  databaseURL: "https://jhajssdsfff-default-rtdb.firebaseio.com",
+  projectId: "jhajssdsfff",
+  storageBucket: "jhajssdsfff.appspot.com",
+  messagingSenderId: "217459285731",
+  appId: "1:217459285731:web:17f0f1fc7ef35a2a5e6fd6"
+  };
+
